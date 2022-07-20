f4c6b7880990732d6f01e292032478f1aec72402
Albert Albala <albert@mediatribe.net>
2022-07-19 21:12:56 -0400

    Move login functionality to its own module (#20)

--- app/code/app.js
+++ app/code/app.js
@@ -207,39 +207,6 @@ class App {
     expressApp.use(this.component('./authentication/index.js').passport().initialize());
     expressApp.use(this.component('./authentication/index.js').passport().session());
 
-    expressApp.get('/login',
-      (req, res) => res.sendFile('login.html',
-      { root: '/usr/src/app/private' })
-    );
-
-    expressApp.post('/login', (req, res, next) => {
-      this.component('./authentication/index.js').passport().authenticate('local',
-      (err, user, info) => {
-        if (err) {
-          console.log('error during /login');
-          console.log(err);
-          return next(err);
-        }
-
-        if (!user) {
-          console.log('no user during /login');
-          console.log(info);
-          return res.redirect('/login?info=' + info);
-        }
-
-        req.logIn(user, function(err) {
-          console.log('There is a user, we are logging in');
-          console.log(user);
-          if (err) {
-            return next(err);
-          }
-
-          return res.redirect('/');
-        });
-
-      })(req, res, next);
-    });
-
     this.eachComponent(async function(component) {
       if (typeof that.component(component).run === 'function') {
         console.log('[x] ' + component + ' has a run() function; calling it.');
--- app/code/userPassAuth/index.js
+++ app/code/userPassAuth/index.js
@@ -11,6 +11,45 @@ class UserPassAuth extends require('../component/index.js') {
     ];
   }
 
+  async run(
+    app /*:: : Object */
+  ) /*:: : Object */ {
+
+    const expressApp = app.component('./express/index.js').expressApp();
+
+    expressApp.get('/login',
+      (req, res) => res.sendFile('login.html',
+      { root: '/usr/src/app/private' })
+    );
+
+    expressApp.post('/login', (req, res, next) => {
+      app.component('./authentication/index.js').passport().authenticate('local',
+      (err, user, info) => {
+        if (err) {
+          console.log('error during /login');
+          console.log(err);
+          return next(err);
+        }
+
+        if (!user) {
+          console.log('no user during /login');
+          console.log(info);
+          return res.redirect('/login?info=' + info);
+        }
+
+        req.logIn(user, function(err) {
+          console.log('There is a user, we are logging in');
+          console.log(user);
+          if (err) {
+            return next(err);
+          }
+
+          return res.redirect('/');
+        });
+      })(req, res, next);
+    });
+  }
+
 }
 
 // $FlowExpectedError
